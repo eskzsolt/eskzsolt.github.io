00c4a28b96fba526d616c54f361b5b9cc2a1c2fa
Zsolt Eskulits <eskzsolt@gmail.com>
2018-07-11 16:06:44 +0200

    Project images zoom fixed

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-$("#scroll-down").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".projects").offset().top},
-        800);
-});
+(function() {
+  $(function() {
 
-$("#scroll-up").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".main").offset().top},
-        800);
-});
+    $("#scroll-down").click(function() {
+      $('html,body').animate({
+        scrollTop: $(".projects").offset().top}, 800);
+    });
 
-$('.button').click(function() {
-    document.activeElement.blur();
-});
+    $("#scroll-up").click(function() {
+      $('html,body').animate({
+        scrollTop: $(".main").offset().top}, 800);
+    });
+
+    $('.button').click(function() {
+      document.activeElement.blur();
+    });
+
+    $('#rejto-ipsum').click(function() {
+      window.open('https://eskzsolt.github.io/rejto-ipsum/','mywindow');
+    });
+    $('#animated-abugida').click(function() {
+      window.open('https://eskzsolt.github.io/animated-abugida/','mywindow');
+    });
+
+  });
+})();
