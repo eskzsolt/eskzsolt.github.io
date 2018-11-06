59419a23262fc4302bf9bdfa11fd5957caa61c69
Zsolt Eskulits <eskzsolt@gmail.com>
2018-11-06 20:09:41 +0100

    StandardJS compliance

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-(function() {
-
-  $(function() {
-
-    $("#scroll-down").click(function() {
+(function () {
+  const $ = window.$
+  $(function () {
+    // Navigation
+    $('#scroll-down').click(function () {
       $('html,body').animate({
-        scrollTop: $(".projects").offset().top}, 800);
-    });
-
-    $("#scroll-up").click(function() {
+        scrollTop: $('.projects').offset().top }, 800)
+    })
+    $('#scroll-up').click(function () {
       $('html,body').animate({
-        scrollTop: $(".main").offset().top}, 800);
-    });
-
-    $('.button').click(function() {
-      document.activeElement.blur();
-    });
-
-    $('.box-inner').click(function() {
+        scrollTop: $('.main').offset().top }, 800)
+    })
+    $('.button').click(function () {
+      document.activeElement.blur()
+    })
+    $('.box-inner').click(function () {
       window.open(`https://eskzsolt.github.io/${this.id}`)
     })
-
-  });
-
-})();
+    // TODO: Auto time zone correction
+    /*
+      Since 1996 European Summer Time has been observed between 1:00 UTC (2:00 CET and 3:00 CEST) on the last Sunday of March
+      and 1:00 UTC on the last Sunday of October; previously the rules were not uniform across the European Union.
+    */
+  })
+})()
