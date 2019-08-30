8c103cde60f46409d04f1d5c3e0aab77e2dd2b63
Zsolt Eskulits <eskzsolt@gmail.com>
2019-08-30 07:42:46 +0200

    JS cleanup

--- script.js
+++ script.js
@@ -3,12 +3,10 @@
   $(function () {
     // Navigation
     $('#scroll-down').click(function () {
-      $('html,body').animate({
-        scrollTop: $('.projects').offset().top }, 800)
+      $('html,body').animate({ scrollTop: $('.projects').offset().top }, 800)
     })
     $('#scroll-up').click(function () {
-      $('html,body').animate({
-        scrollTop: $('.main').offset().top }, 800)
+      $('html,body').animate({ scrollTop: $('.main').offset().top }, 800)
     })
     $('.button').click(function () {
       document.activeElement.blur()
